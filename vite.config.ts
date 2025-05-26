e96ffdf4968ab41b0d4e3b9ff7a83606ee3b7183
Ulico <aruss8@illinois.edu>
2025-05-26 01:47:56 -0500

    fix: ensure base path is set for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/', // Set base to root for user/organization GitHub Pages
   plugins: [react()],
 })
